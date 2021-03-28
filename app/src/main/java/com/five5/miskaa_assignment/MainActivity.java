d54d1a5a3386c6fd7c0e212fc06299823a2ca922
Pravesh Choudhary <praveshmails@gmail.com>
2021-03-29 01:05:00 +0530

    First Commit

---
+++ app/src/main/java/com/five5/miskaa_assignment/MainActivity.java
@@ -0,0 +1,16 @@
+package com.five5.miskaa_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.five5.miskaa_assignment.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
